0328ef145ce367ec1792569ddb88e9fbc66b8ed6
zhangdaxing <zhangdaxing@moqipobing.com>
2025-03-27 11:32:41 +0800

    spine的问题

---
+++ assets/Scripts/SpineBatch/SpineBatchMain.ts
@@ -0,0 +1,12 @@
+const { ccclass, property } = cc._decorator;
+
+@ccclass('SpineMaterialMerger')
+export class SpineMaterialMerger extends cc.Component {
+    onLoad() {
+
+    }
+
+    protected start(): void {
+       
+    }
+}
